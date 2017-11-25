9fcabe382178535ada3841fecb0019e1820ecafc
gutzofter <gutzofter@alephchain.com>
2017-11-25 07:37:44 -0800

    added age property

--- agent.py
+++ agent.py
@@ -19,6 +19,17 @@ class Agent:
             self.score.append(payoff)
             self.returns = new_return
 
+    def get_age(self):
+        age = 0
+        sum_score = 1000
+        for i in self.score:
+            age += 1
+            sum_score += self.score[i]
+            if sum_score == 0:
+                return age
+
+        return age
+
     def get_score(self):
         return self.returns
 
--- run_context.py
+++ run_context.py
@@ -5,26 +5,30 @@ import csv
 from agent import *
 from payoff import *
 
-max_agents = 400
+max_agents = 1000
 strategies = 4
 odds_calc = 4
 steps = (2 ** 12)
 
 agents = []
 
-for i in range(max_agents * odds_calc):
-    agents.append(Agent(i, i % odds_calc, i % strategies))
+i = 0
+for a in range(max_agents):
+    for o in range(odds_calc):
+        for s in range(strategies):
+            agents.append(Agent(i, o, s))
+            i += 1
 
-for i in range(steps * max_agents * odds_calc):
-    agent_idx = i % (max_agents * odds_calc)
+for i in range(steps * max_agents * odds_calc * strategies):
+    agent_idx = i % (max_agents * odds_calc * strategies)
     payoff(agents[agent_idx])
 
 with open(r'C:\Users\gutzofter\PycharmProjects\riskaverse\data\strategy.csv', 'wb') as f:
     writer = csv.writer(f, delimiter=',')
-    writer.writerow(['strategy', 'odds', 'score'])
+    writer.writerow(['strategy', 'age', 'odds', 'score'])
 
-    for i in range(max_agents * odds_calc):
-        print agents[i].idx, agents[i].strategy_label(), agents[i].odds_label(), agents[i].get_score()
-        writer.writerow([agents[i].strategy_label(), agents[i].odds_label(), agents[i].get_score()])
+    for i in range(max_agents * odds_calc * strategies):
+        print agents[i].idx, agents[i].get_age(), agents[i].strategy_label(), agents[i].odds_label(), agents[i].get_score()
+        writer.writerow([agents[i].strategy_label(), agents[i].get_age(), agents[i].odds_label(), agents[i].get_score()])
 
-print "Agents: " + str(max_agents * odds_calc), "Steps: " + str(steps)
+print "Agents: " + str(max_agents * odds_calc * strategies), "Steps: " + str(steps)
